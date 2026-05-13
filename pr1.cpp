79933229edabfe91104ae209a2fd4a8440d7383a
Agabriel77718 <00199526@uca.edu.sv>
2026-05-12 23:57:08 -0600

    practicando git

---
+++ pr1.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+int numero;
+
+cout<<"porfavor ingresa un numero"<<endl;
+cin>>numero;
+
+
+if (numero>=60)
+{
+    cout<<"si es mayor que 60"<<endl;
+}
+else{
+cout <<"no es mayor que 60"<<endl;
+
+
+}
+
+
+
+
+
+}
